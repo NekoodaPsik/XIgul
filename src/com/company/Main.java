40c4b818fd84cd8595a1c5522a2f4bf7f5efff6e
NekoodaPsik <nekooda.psik2020@gmail.com>
2020-10-15 15:51:49 +0300

    Tutorial 06 while and for
    Tutorial 06 while and for

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,20 +8,54 @@ public class Main {
 	    // write your code here
         System.out.println("Welcome to my game");
 
-        String name;
-        int age;
-
-        // The object we use to read the console input
+        int topNumber = 100;
+        int userInput = 0;
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter you name");
-        name = scanner.next();
-        System.out.println(name);
+        /*
+         * while expression is similar to if except the code between { } runs
+         * until the condition of the while is false
+         */
+        //The user need to enter number that is bigger than topNumber, until he enters it
+        //the program will ask him to enter a number
 
+        while(userInput < topNumber){
+            System.out.println("Enter number:");
+            userInput = scanner.nextInt();
+        }
 
-        System.out.println("Enter you age");
-        age = scanner.nextInt();
-        System.out.println(age);
+        /*
+         * The for expression structure is:
+         * for(line of code that runs once usually declare a variable;
+         *     condition;
+         *     line of code that runs after each execution of the code in the parentheses) { code }
+         */
+
+        //In this mini game the player has 3 attempts to enter a number that was predetermined
+
+        int number = 5;
+        boolean isCorrectNumber = false;
+        for(int i=0; !isCorrectNumber && i < 3; i++){
+            System.out.println("Enter number between 0-9");
+            userInput = scanner.nextInt();
+            if(userInput == number){
+                System.out.println("You have entered the correct number");
+                isCorrectNumber = true;
+            }
+        }
+
+        //while version of the above for
+        int index = 0;
+        isCorrectNumber = false;
+        while (!isCorrectNumber && index < 3){
+            System.out.println("Enter number between 0-9");
+            userInput = scanner.nextInt();
+            if(userInput == number){
+                System.out.println("You have entered the correct number");
+                isCorrectNumber = true;
+            }
+            index++;
+        }
 
     }
 }
