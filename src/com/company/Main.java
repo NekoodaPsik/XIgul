5a084915ab8a59c7a9414c9e514de4b4df296c76
NekoodaPsik <nekooda.psik2020@gmail.com>
2020-10-15 15:30:27 +0300

    Tutorial 05 user input and String
    Tutorial 05 user inout and String

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,61 +1,27 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	    // write your code here
         System.out.println("Welcome to my game");
 
-        // The game
-        /*
-         * boolean type can have 2 values: true or false
-         */
-        boolean flag = true;
+        String name;
+        int age;
 
-        /*
-         * Conditions:
-         * As you can recognize from this word conditions are statement that yield true or false
-         * for example : 4 is smaller than 6 is true
-         * We have several operators that if used yield a boolean value they are :
-         * >, <, == (equality), ! (not), != (not equal), <= (smaller or equal), || (or), && (and)
-         * e.g the previous example 4 < 6 ==> true
-         */
-        int number1 = 4, number2 = 6;
-        flag = number1 < number2;
+        // The object we use to read the console input
+        Scanner scanner = new Scanner(System.in);
 
-        /*
-         * In order to do something useful with this boolean value we need an expression
-         * which says do something if the value is true and that where if expression comes in
-         * to play
-         */
+        System.out.println("Enter you name");
+        name = scanner.next();
+        System.out.println(name);
 
-        if(flag){
-            System.out.println("The flag is true");
-        }
 
-        /*
-         * we can add an else expression after the code of the if in order
-         * to execute the code if the condition is not true
-         */
+        System.out.println("Enter you age");
+        age = scanner.nextInt();
+        System.out.println(age);
 
-        if(number1 == number2){
-            System.out.println("number1 and number2 are equal");
-        } else {
-            System.out.println("number1 and number2 are not equal");
-        }
-
-        /*
-         * more complex example
-         */
-        int y = 10;
-        // if the flag is true and y < 12
-        if(flag && y < 12){
-            if(number1 == 7){
-                System.out.println("number1 less than number2 and y < 12 and number1 equal to 7, therefore we add 90 to number 1");
-                number1 = number1 + 90;
-            }else{
-                System.out.println("number1 less than number2 and y < 12 and number1 not equal to 7 we dont change the value of number1");
-            }
-        }
     }
 }
