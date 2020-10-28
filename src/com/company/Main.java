b4718e5e55391357c8eec7df3905c3573f765930
NekoodaPsik <nekooda.psik2020@gmail.com>
2020-10-28 10:10:03 +0200

    Tutorial 07 arrays
    Tutorial 07 1D and 2D arrays

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,56 +5,47 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	    // write your code here
-        System.out.println("Welcome to my game");
-
-        int topNumber = 100;
-        int userInput = 0;
-        Scanner scanner = new Scanner(System.in);
 
         /*
-         * while expression is similar to if except the code between { } runs
-         * until the condition of the while is false
+         * We are creating an array of size 5 that hold only integers
+         * Arrays are continuous memory you can invasion them as follows:
+         *         index     0   1   2   3
+         *         array  | 1 | 10 | 4 | 5 |
+         * To change or get a value in the array we need to use the [] operator
+         * Set the value of index 1 in the array (the second member) varName[1] = value
          */
-        //The user need to enter number that is bigger than topNumber, until he enters it
-        //the program will ask him to enter a number
+        int[] intArray = new int[4];
+        intArray[0] = 1;
+        intArray[1] = 10;
+        intArray[2] = 4;
+        intArray[3] = 5;
 
-        while(userInput < topNumber){
-            System.out.println("Enter number:");
-            userInput = scanner.nextInt();
+        System.out.println("1D array:");
+        // Print all the values in the array
+        for(int i=0; i < intArray.length; ++i){
+            System.out.println(intArray[i]);
         }
 
         /*
-         * The for expression structure is:
-         * for(line of code that runs once usually declare a variable;
-         *     condition;
-         *     line of code that runs after each execution of the code in the parentheses) { code }
+         * 2D array or array of arrays
+         * int[][] is the type were the first [] is the index of rows and
+         * the second [] is the index of the columns for example
+         * int[][] array2D = new int[3][3]  array with 3 rows and 3 columns
+         * array2D[1][2] the value in the second row(index 1) and the third column(index 2)
          */
+        int[][] int2DArray = new int[3][3];
 
-        //In this mini game the player has 3 attempts to enter a number that was predetermined
+        int2DArray[0][0] = 1; int2DArray[0][1] = 2; int2DArray[0][2] = 3;
+        int2DArray[1][0] = 4; int2DArray[1][1] = 5; int2DArray[1][2] = 6;
+        int2DArray[2][0] = 7; int2DArray[2][1] = 8; int2DArray[2][2] = 9;
 
-        int number = 5;
-        boolean isCorrectNumber = false;
-        for(int i=0; !isCorrectNumber && i < 3; i++){
-            System.out.println("Enter number between 0-9");
-            userInput = scanner.nextInt();
-            if(userInput == number){
-                System.out.println("You have entered the correct number");
-                isCorrectNumber = true;
+        System.out.println("\n2D array:");
+        // Print all the values in the 2D array
+        for(int i=0; i < int2DArray.length; i++){
+            for(int j=0; j < int2DArray[0].length; j++) {
+                System.out.print(int2DArray[i][j]+ " ");
             }
-        }
-
-        //while version of the above for
-        int index = 0;
-        isCorrectNumber = false;
-        while (!isCorrectNumber && index < 3){
-            System.out.println("Enter number between 0-9");
-            userInput = scanner.nextInt();
-            if(userInput == number){
-                System.out.println("You have entered the correct number");
-                isCorrectNumber = true;
-            }
-            index++;
+            System.out.println();
         }
 
     }
